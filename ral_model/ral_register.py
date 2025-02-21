11a7a22df41260fd6b3559be089d75bc7cea284c
LitchiKnight <572415094@qq.com>
2025-02-21 16:20:20 +0800

    add reset value compare

--- ral_model/ral_register.py
+++ ral_model/ral_register.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 from common.base import Base
 from ral_model.ral_base import RalBase
@@ -21,12 +22,26 @@ class RalRegister(RalBase):
         if not self.fields:
             Base.error("There is no field in register, please check!")
         return self.fields[-1]
-    
+
     def verify_fields(self) -> None:
         """Verify if the fields are valid."""
         total_filed_bits = sum([int(f.bits) for f in self.fields])
         if total_filed_bits != int(self.width):
             Base.error(f"The sum of all field bits in register {self.name} is {total_filed_bits}, not equal to its bit width {self.width}, please check!")
+
+        PREFIX = re.compile(r"(^[1-9]+[0-9]*\'b)|(^[1-9]+[0-9]*\'h)")
+        compact_reset = 0
+        rg_reset = int(PREFIX.sub("", self.reset), 16)
+        offset = 0
+
+        for f in reversed(self.fields):
+            if not f.reserved:
+                field_reset = int(PREFIX.sub("", f.reset), 16)
+                compact_reset += field_reset << offset
+            offset += int(f.bits)
+
+        if compact_reset != rg_reset:
+            Base.error(f"The compact fields reset value {hex(compact_reset)} of register {self.name} is not equal to its reset value {hex(rg_reset)}, please check!")
 
     def gen_ralf_code(self) -> str:
         """Generate RALF code for the register."""
